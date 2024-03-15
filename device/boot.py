8e000599f71a9384b58012fe14c21af7494afe7e
Alexis BRENON <brenon.alexis@gmail.com>
2024-03-15 15:41:15 +0100

    UC1701 first success

---
+++ device/boot.py
@@ -0,0 +1,9 @@
+# This file is executed on every boot (including wake-boot from deepsleep)
+#import esp
+#esp.osdebug(None)
+#import webrepl
+#webrepl.start()
+
+import machine
+
+machine.Pin(2, machine.Pin.OUT, value=0)
--- device/uc1701.py
+++ device/uc1701.py
@@ -10,17 +10,17 @@ import asyncio
 
 SET_COLUMN_ADDRESS_LSB = const(0x00)
 SET_COLUMN_ADDRESS_MSB = const(0x10)
-SET_POWER_CONTROL = const(0x28)
-SET_SCROLL_LINE = const(0x40)
+SET_POWER_CONTROL = const(0x28) # 0b0010 1###
+SET_SCROLL_LINE = const(0x40) # 0b01## ####
 SET_PAGE_ADDRESS = const(0xB0)
-SET_VLCD_RESISTOR_RATIO = const(0x20)
-SET_ELECTRONIC_VOLUME_CMD = const(0x81)
-SET_ELECTRONIC_VOLUME_VALUE = const(0x20)
+SET_VLCD_RESISTOR_RATIO = const(0x20) # 0b0010 0###
+SET_ELECTRONIC_VOLUME_CMD = const(0x81) # Set Contrast
+SET_ELECTRONIC_VOLUME_VALUE = const(0x00) # 0b00## ####
 SET_ALL_PIXEL_ON = const(0xA4)
-SET_INVERSE_DISPLAY = const(0xA6)
-SET_DISPLAY_ENABLE = const(0xAE)
-SET_SEG_DIRECTION = const(0xA0)
-SET_COM_DIRECTION = const(0xC0)
+SET_INVERSE_DISPLAY = const(0xA6) # 0b1010 011#
+SET_DISPLAY_ENABLE = const(0xAE) # 0b1010 111#
+SET_SEG_DIRECTION = const(0xA0) # 0b1010 000#
+SET_COM_DIRECTION = const(0xC0) # 0b1100 #---
 SYSTEM_RESET = const(0xE2)
 NOP = const(0xE3)
 SET_LCD_BIAS_RATIO = const(0xA2)
@@ -31,93 +31,114 @@ SET_STATIC_INDICATOR_ON = const(0xAD)
 SET_BOOSTER_RATIO_1 = const(0xF8)
 SET_BOOSTER_RATIO_2 = const(0x00)
 SET_POWER_SAVE = const(0x00)
-SET_ADV_PROGRAM_CONTROL_0_1 = const(0xFA)
-SET_ADV_PROGRAM_CONTROL_0_2 = const(0x90)
+SET_ADV_PROGRAM_CONTROL_0_CMD = const(0xFA) # 0b1111 1010
+SET_ADV_PROGRAM_CONTROL_0_VALUE = const(0x00) # 0b#0## 00##
+SET_ADV_PROGRAM_CONTROL_0_VALUE_LSHIFT_TC = const(7)
+SET_ADV_PROGRAM_CONTROL_0_VALUE_LSHIFT_FR = const(4)
+SET_ADV_PROGRAM_CONTROL_0_VALUE_LSHIFT_WA = const(0)
+
+SPECIFICATION_POWER_UP_SEQUENCE = (
+    SET_SEG_DIRECTION | 0x0,
+    SET_COM_DIRECTION | 0x0,
+    SET_VLCD_RESISTOR_RATIO | 0b011,
+    SET_LCD_BIAS_RATIO | 0x0,
+    SET_ELECTRONIC_VOLUME_CMD,
+    SET_ELECTRONIC_VOLUME_VALUE | 55,
+    SET_POWER_CONTROL | 0b111,
+    SET_DISPLAY_ENABLE | 0x1,
+)
+
+_NULL_PIN = lambda x: x
 
 # Subclassing FrameBuffer provides support for graphics primitives
 # http://docs.micropython.org/en/latest/pyboard/library/framebuf.html
 class UC1701(framebuf.FrameBuffer):
-    def __init__(self):
+    def __init__(self, *, br: int = 0b0, pc: int = 0b100, rst: machine.Pin = _NULL_PIN):
         self.width = 128
         self.height = 64
         self.buffer = bytearray(64 * 132 // 8)
-        super().__init__(self.buffer, self.width, self.height, framebuf.MONO_VLSB, 132)
+        self.rst = rst
+        self._br = br
+        self._pc = pc
+        super().__init__(self.buffer, self.width, self.height, framebuf.MONO_VLSB)
 
-    def write_cmd(self, cmd: int):
+    async def write_cmd(self, cmd: int):
         raise NotImplementedError
 
-    def write_data(self, data: bytes):
+    async def write_data(self, data: bytes):
         raise NotImplementedError
 
     async def power_up(self):
-        for cmd in (
-            SET_SEG_DIRECTION | 0x1,
-            SET_COM_DIRECTION | 0x0,
-            SET_VLCD_RESISTOR_RATIO | 0x3,
-            SET_LCD_BIAS_RATIO | 0x0,
-            SET_ELECTRONIC_VOLUME_CMD,
-            SET_ELECTRONIC_VOLUME_VALUE | 0x8,
-        ):
-            self.write_cmd(cmd)
-        await self.show()
-        self.write_cmd(SET_POWER_CONTROL | 0x1)
-        self.write_cmd(SET_DISPLAY_ENABLE | 0x1)
+        await self.power_down()
+        await self.set_x_mirror(False)
+        await self.set_y_mirror(False)
+        await self.write_cmd(SET_VLCD_RESISTOR_RATIO | (0b111 & self._pc))
+        await self.write_cmd(SET_LCD_BIAS_RATIO | (0b1 & self._br))
+        await self.write_cmd(SET_POWER_CONTROL | 0b111)
+        await self.wake_up()
 
     async def power_down(self):
-        self.write_cmd(SYSTEM_RESET)
+        await self.write_cmd(SYSTEM_RESET)
+        await asyncio.sleep_ms(5)
+        self.rst(0)
+        await asyncio.sleep_ms(1)
+        self.rst(1)
         await asyncio.sleep_ms(5)
 
     async def sleep(self):
-        self.write_cmd(SET_DISPLAY_ENABLE | 0x0)
-        self.write_cmd(SET_ALL_PIXEL_ON | 0x1)
+        await self.write_cmd(SET_DISPLAY_ENABLE | 0x0)
+        await self.write_cmd(SET_ALL_PIXEL_ON | 0x1)
 
     async def wake_up(self):
-        self.write_cmd(SET_ALL_PIXEL_ON | 0x0)
-        self.write_cmd(SET_DISPLAY_ENABLE | 0x1)
-
-    async def invert(self, invert):
-        self.write_cmd(SET_INVERSE_DISPLAY | (invert & 0x1))
+        await self.write_cmd(SET_ALL_PIXEL_ON | 0x0)
+        await self.write_cmd(SET_DISPLAY_ENABLE | 0x1)
 
     async def show(self):
-        self.write_cmd(SET_PAGE_ADDRESS | 0x0)
-        self.write_cmd(SET_COLUMN_ADDRESS_MSB | 0x0)
-        self.write_cmd(SET_COLUMN_ADDRESS_LSB | 0x0)
-        self.write_data(self.buffer)
+        for page in range(8):
+            await self.write_cmd(SET_PAGE_ADDRESS | (0xF & page))
+            await self.write_cmd(SET_COLUMN_ADDRESS_MSB | 0x0)
+            await self.write_cmd(SET_COLUMN_ADDRESS_LSB | 0x0)
+            start = self.width * page
+            end = start + self.width
+            await self.write_data(self.buffer[start:end + 1])
+
+    async def set_x_mirror(self, value: bool):
+        await self.write_cmd(SET_SEG_DIRECTION | (0b1 & value))
+
+    async def set_y_mirror(self, value: bool):
+        await self.write_cmd(SET_COM_DIRECTION | ((0b1 & value) << 3))
+
+    async def set_inverse(self, value: bool):
+        await self.write_cmd(SET_INVERSE_DISPLAY | (0b1 & value))
+
+    async def set_advance_program(self, tr: bool, fr: int, wa: int):
+        await self.write_cmd(SET_ADV_PROGRAM_CONTROL_0_CMD)
+        await self.write_cmd(
+            SET_ADV_PROGRAM_CONTROL_0_VALUE |
+            ((0b1 & tr) << 7) |
+            ((0b11 & fr) << 4) |
+            ((0b11 & wa) << 0)
+        )
+
+    async def set_contrast(self, level: int | float | None = None, *, pm: int | None = None):
+        if level is None and pm is None:
+            raise ValueError("One of value or pm should be specified")
+        if level is not None:
+            if isinstance(level, int):
+                level = level / 255
+            pm = int(level * 0b111111)
+        await self.write_cmd(SET_ELECTRONIC_VOLUME_CMD)
+        await self.write_cmd(SET_ELECTRONIC_VOLUME_VALUE | (0b111111 & pm))
 
 class UC1701_SPI(UC1701):
-    def __init__(self, spi: machine.SPI, cd: machine.Pin, rst: machine.Pin | None = None, cs0: machine.Pin | None = None):
+    def __init__(self, *, spi: machine.SPI, cd: machine.Pin, cs0: machine.Pin = _NULL_PIN, **kwargs):
         self.rate = 10 * 1024 * 1024
         self.spi = spi
         self.dc = cd
-        self.dc.init(machine.Pin.OUT, value=0)
+        self.cs = cs0
+        super().__init__(**kwargs)
 
-        if rst is not None:
-            rst.init(machine.Pin.OUT, value=0)
-        self.res = rst or (lambda x: x)
-
-        if cs0 is not None:
-            cs0.init(machine.Pin.OUT, value=1)
-        self.cs = cs0 or (lambda x: x)
-        super().__init__()
-
-    async def power_up(self):
-        self.res(1)
-        await asyncio.sleep_ms(1)
-        self.res(0)
-        await asyncio.sleep_ms(1)
-        self.res(1)
-        await asyncio.sleep_ms(5)
-
-        return await super().power_up()
-
-    async def power_down(self):
-        await super().power_down()
-        self.res(0)
-        await asyncio.sleep_ms(1)
-        self.res(1)
-        await asyncio.sleep_ms(5)
-
-    def write_cmd(self, cmd):
+    async def write_cmd(self, cmd):
         self.spi.init(baudrate=self.rate, polarity=0, phase=0)
         self.cs(1)
         self.dc(0)
@@ -125,7 +146,7 @@ class UC1701_SPI(UC1701):
         self.spi.write(bytearray([cmd]))
         self.cs(1)
 
-    def write_data(self, buf):
+    async def write_data(self, buf):
         self.spi.init(baudrate=self.rate, polarity=0, phase=0)
         self.cs(1)
         self.dc(1)
