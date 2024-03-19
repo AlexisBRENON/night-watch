3f4eac67752952c30245613ed7f4756fbfd83c60
Alexis BRENON <brenon.alexis@gmail.com>
2024-03-19 08:26:34 +0100

    feat(click): Working solution

--- device/click.py
+++ device/click.py
@@ -6,76 +6,117 @@ import micropython
 
 
 class Click:
-    NONE = micropython.const("")
+    _PRESS = micropython.const((True, False, False))
+    _LONGPRESS = micropython.const((True, True, False))
+    _RELEASE = micropython.const((False, False, True))
+    _NONE = micropython.const((False, False, False))
+    _STOPPED = micropython.const((True, True, True))
+
+    _EVENT = micropython.const("?")
     SHORT = micropython.const(".")
     LONG = micropython.const("_")
 
     def __init__(
         self,
         pin: machine.Pin,
-        timer: machine.Timer | None = None,
-        bounce_time: int = 25,
-        trigger: int = machine.Pin.IRQ_FALLING,
+        timer: machine.Timer = machine.Timer(0),
+        long_ms: int = 500,
+        invert: bool = False,
     ):
-        self._signal = machine.Signal(pin, trigger == machine.Pin.IRQ_RISING)
-        self._timer = timer or machine.Timer(-1)
-        self._bounce = bounce_time
+        self._pin = pin
+        self._timer = timer
+        self._long_ms = long_ms
+        self._short_ms = long_ms // 3
+        self._bounce = self._short_ms // 5
+        self._invert = invert
+
         self._debounce_start = time.ticks_ms()
         self._debounce_end = time.ticks_ms()
-        self._signal.irq(self._on_press, machine.Signal.IRQ_FALLING)
-        self._signal.irq(self._on_release, machine.Signal.IRQ_RISING)
+
+        self._update_state = asyncio.ThreadSafeFlag()
 
         self.is_pressed = asyncio.Event()
         self.is_long = asyncio.Event()
-        self._set_is_long_cb = self.is_long.set
         self.is_released = asyncio.Event()
+        self._events = (self.is_pressed, self.is_long, self.is_released)
 
-        self._events = [self.NONE] * 5
-        self._event_count = 0
+        self._state = self._NONE
+        self._clicks = []
+
+        self._pin.irq(self._debounce, machine.Pin.IRQ_FALLING | machine.Pin.IRQ_RISING)
+
+    async def handle(self):
+        while self._state is not self._STOPPED:
+            print(self)
+            try:
+                await asyncio.wait_for(self._update_state.wait(), 1000)
+            except TimeoutError:
+                continue
+
+            for state, event in zip(self._state, self._events):
+                event.set() if state else event.clear()
+
+    def stop(self):
+        self._state = self._STOPPED
 
     @property
-    def events(self) -> tuple[str]:
-        return tuple(self._events[0 : self._event_count])
+    def clicks(self) -> tuple[str]:
+        return tuple(self._clicks)
 
-    def _on_press(self, pin: machine.Pin):
+    def _debounce(self, pin: machine.Pin):
         self._debounce_end = time.ticks_ms()
         if time.ticks_diff(self._debounce_end, self._debounce_start) > self._bounce:
-            self._debounce_start = self._debounce_end
+            if pin.value() == self._invert:
+                micropython.schedule(Click._on_press, self)
+            else:
+                micropython.schedule(Click._on_release, self)
+        self._debounce_start = self._debounce_end
 
-            self.is_released.clear()
-            self.is_pressed.set()
-            self._timer.init(
-                mode=self._timer.ONE_SHOT,
-                period=3 * self._bounce,
-                callback=self.is_long.set,
-            )
+    def _set_is_long_cb(self, _: machine.Timer):
+        self._state = self._LONGPRESS
+        self._update_state.set()
 
-    def _on_release(self, pin: machine.Pin):
-        self._debounce_end = time.ticks_ms()
-        if time.ticks_diff(self._debounce_end, self._debounce_start) > self._bounce:
-            self._debounce_start = self._debounce_end
-
-            self._timer.deinit()
-            if self.is_pressed.is_set():
-                if self.is_long.is_set():
-                    self._events[self._event_count] = self.LONG
-                else:
-                    self._events[self._event_count] = self.SHORT
-                self.is_long.clear()
-            self.is_pressed.clear()
-            self._event_count += 1
-
-            self.is_released.set()
-            self._timer.init(
-                mode=self._timer.ONE_SHOT,
-                period=7 * self._bounce,
-                callback=self._on_event_end,
-            )
-
-    def _on_event_end(self, timer: machine.Timer):
+    def _on_press(self):
         self._timer.deinit()
-        self.is_pressed.clear()
-        self.is_released.clear()
-        self.is_long.clear()
-        self._events[:] = self.NONE
-        self._event_count = 0
+        self._state = self._PRESS
+        self._update_state.set()
+        self._timer.init(
+            mode=self._timer.ONE_SHOT,
+            period=self._long_ms,
+            callback=self._set_is_long_cb,
+        )
+
+    def _on_release(self):
+        self._timer.deinit()
+        if self._state in {self._PRESS, self._LONGPRESS}:
+            self._clicks.append(
+                self.LONG if self._state == self._LONGPRESS else self.SHORT
+            )
+
+        self._state = self._RELEASE
+        self._update_state.set()
+        self._timer.init(
+            mode=self._timer.ONE_SHOT,
+            period=5 * self._short_ms,
+            callback=self._on_event_end,
+        )
+
+    def _on_event_end(self, _: machine.Timer):
+        self._timer.deinit()
+        self._clicks.clear()
+        self._state = self._NONE
+        self._update_state.set()
+
+    def __str__(self) -> str:
+        return f"Click({self._pin}):\n\tpressed (long): {self.is_pressed.is_set()} ({self.is_long.is_set()}),\n\treleased: {self.is_released.is_set()}\n\tevents: {self.clicks}"
+
+
+if __name__ == "__main__":
+    import c3pico
+
+    h = Click(c3pico.button)
+
+    asyncio.run(h.handle())
+
+
+# import click; import c3pico; import asyncio; h = click.Click(c3pico.button); asyncio.run(h.handle())
--- device/test.py
+++ device/test.py
@@ -1,36 +1,47 @@
 import asyncio
-import c3pico
 import time
 
 import ntptime
 
-import click
 import wlan
+
+import c3pico
+import click
 
 click_handler = click.Click(c3pico.button)
 
-i = 0
-c3pico.rgb_led(i, i, i)
-while True:
-    print("Waiting for press...")
-    asyncio.run(click_handler.is_pressed.wait())
-    print("Passed events: ", click_handler.events)
-    i = 10 * (len(click_handler.events) + 1)
+
+async def test_click():
+    i = 0
     c3pico.rgb_led(i, i, i)
-    print("Waiting for release...")
-    asyncio.run(click_handler.is_released.wait())
-    print("Registered events: ", click_handler.events)
-    if click_handler.events == (
-        click_handler.LONG,
-        click_handler.LONG,
-        click_handler.LONG,
-    ):
-        c3pico.rgb_led(i, 0, 0)
-        break
+    print(click_handler)
+    while True:
+        print("Waiting for press...", click_handler)
+        await click_handler.is_pressed.wait()
+        i = 10 * (len(click_handler.events) + 1)
+        c3pico.rgb_led(i, i, i)
+        print("Waiting for release...", click_handler)
+        await click_handler.is_released.wait()
+        if click_handler.events == (
+            click_handler.LONG,
+            click_handler.LONG,
+            click_handler.LONG,
+        ):
+            c3pico.rgb_led(i, 0, 0)
+            break
+        print("Continue...")
 
-print("Done")
+    print("Done")
 
-net_connection = asyncio.run(wlan.get_instance())
-if net_connection.config("ssid") != wlan.AP_SSID:
-    ntptime.settime(timezone=0, server="fr.pool.ntp.org")
-    print("UTC time after synchronization: %s" % str(time.localtime()))
+
+async def test_ntp():
+    print("UTC time before synchronization: %s" % str(time.gmtime()))
+    net_connection = await wlan.get_instance()
+    print(net_connection.config("ssid"))
+    if net_connection.config("ssid") != wlan.AP_SSID:
+        ntptime.settime(timezone=0, server="fr.pool.ntp.org")
+        print("UTC time after synchronization: %s" % str(time.gmtime()))
+
+
+if __name__ == "__main__":
+    asyncio.run(test_click())
